6da8345feccdd1eecb0f8d32a60527e64f0c3a81
Alfredo Suárez Romero <48132178+sibokdev@users.noreply.github.com>
2024-02-01 11:51:13 -0600

    Added basic config to establish connection with config server

--- src/main/java/com/polarbookshop/configservice/ConfigServiceApplication.java
+++ src/main/java/com/polarbookshop/configservice/ConfigServiceApplication.java
@@ -2,8 +2,10 @@ package com.polarbookshop.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer // annotation to configure the project as Config server
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
